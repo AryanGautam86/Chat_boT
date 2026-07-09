b5bf26b5f1f4b1966b97e4e744f60562cd3d1057
Aryan Gautam <aryannsamraat086@gmail.com>
2026-07-10 01:23:31 +0530

    Delete Backend directory

--- Backend/tests/test_reverse.py
+++
@@ -1,7 +0,0 @@
-from generated_code import reverse_string
-
-assert reverse_string("abc") == "cba"
-assert reverse_string("") == ""
-assert reverse_string("a") == "a"
-
-print("All reverse_string() tests passed")
